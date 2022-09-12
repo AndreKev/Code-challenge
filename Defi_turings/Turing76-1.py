cc87c0871416461da302dc6b046828b8db033b38
NYEMB NDJEM EONE ANDRE KEVIN <ak650283114@gmail.com>
2022-09-12 17:41:23 +0100

    Turing96 OK

--- Defi_turings/Turing76-1.py
+++ Defi_turings/Turing76-1.py
@@ -8,9 +8,13 @@ def main():
         if valeur == 20142014:
             return (i, j)
         # Descendre en diagonale
-        while j != 1 and valeur != 20142014:
-            j -= 1; i += 1
-            valeur += 1
+        #while j != 1 and valeur != 20142014:
+        #    j -= 1; i += 1
+        #    valeur += 1
+        minim = min(j-1, 20142014-valeur)
+        j -= minim
+        i += minim
+        valeur += minim
         if valeur == 20142014:
             return i, j
         # Descendre en bas
@@ -19,8 +23,16 @@ def main():
         if valeur == 20142013:
             return i, j
         # Remonter en diagonale
-        while i!= 1 and valeur != 20142014:
-            i -= 1; j += 1
-            valeur += 1
+        #while i!= 1 and valeur != 20142014:
+        #    i -= 1; j += 1
+        #    valeur += 1
+        # uncomment the while loop will do exactly ->
+        minim = min(i-1, 20142014-valeur)
+        i -= minim
+        j+= minim
+        valeur += minim
+        # -> the same as this but the code will be slower
         if valeur == 20142014:
             return i, j
+
+print(main())
